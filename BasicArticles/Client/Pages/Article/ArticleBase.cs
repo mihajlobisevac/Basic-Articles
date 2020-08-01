0a3bdf8520f5865872b2639fd8fa1f89b89247f7
Mihajlo Biševac <kajler11@gmail.com>
2020-08-01 17:20:35 +0200

    Added article page

---
+++ BasicArticles/Client/Pages/Article/ArticleBase.cs
@@ -0,0 +1,26 @@
+﻿using BasicArticles.Client.ViewModels.Article;
+using BasicArticles.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicArticles.Client.Pages.Article
+{
+    public class ArticleBase : ComponentBase
+    {
+        [Inject]
+        public IArticleViewModel ArticleService { get; set; }
+
+        [Parameter]
+        public long Id { get; set; }
+
+        public ArticleModel ArticleModel { get; set; } = new ArticleModel();
+
+        protected override async Task OnInitializedAsync()
+        {
+            ArticleModel = await ArticleService.GetArticle(Id);
+        }
+    }
+}
