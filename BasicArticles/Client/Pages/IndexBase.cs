9a590c28f0120fdbadb9b1303a6ba3d2227f285f
Mihajlo Biševac <kajler11@gmail.com>
2020-08-10 14:57:34 +0200

    Index page displays all articles

---
+++ BasicArticles/Client/Pages/IndexBase.cs
@@ -0,0 +1,23 @@
+﻿using BasicArticles.Client.ViewModels.Article;
+using BasicArticles.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicArticles.Client.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        [Inject]
+        public IArticleViewModel ArticleService { get; set; }
+
+        public List<ArticleModel> Articles { get; set; } = new List<ArticleModel>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Articles = await ArticleService.GetArticleList();
+        }
+    }
+}
